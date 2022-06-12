2eb5bb7f520561c159c772e90fd42f5b8c27be61
Maksim Dryutov <max_005_27@inbox.lv>
2022-06-12 23:25:36 +0300

    prettier

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -5,43 +5,34 @@ import { Col, Row, Spinner } from "react-bootstrap";
 import { Movies } from "../components/views/movies/Movies";
 import { AddButton } from "../components/buttons/adds/AddButton";
 
-
-
-
 export const MoviesPage = () => {
+  const contentLink = "movies/";
 
-  const contentLink = 'movies/';
+  const movies = useAppSelector((state) => state.firebase.movies);
+  const loading = useAppSelector((state) => state.firebase.loading);
+  const uid = useAppSelector((state) => state.auth.user.uid);
 
-  const movies = useAppSelector(state => state.firebase.movies);
-  const loading = useAppSelector(state => state.firebase.loading);
-  const uid = useAppSelector(state => state.auth.user.uid);
+  const isFullOption =
+    process.env.REACT_APP_USER_UID === uid ||
+    process.env.REACT_APP_USER_UID_ZHEN === uid;
 
-  const isFullOption = 
-  (process.env.REACT_APP_USER_UID === uid || process.env.REACT_APP_USER_UID_ZHEN === uid);
-
-    useEffect(() => {
-      if (!movies.length) {
-        fetchFromRealtimeDB(contentLink)
-      }
-    },[movies])
-
+  useEffect(() => {
+    if (!movies.length) {
+      fetchFromRealtimeDB(contentLink);
+    }
+  }, [movies]);
 
   return (
-
     <div className="page-movies">
-
       {loading && <Spinner variant="secondary" animation="grow" />}
 
-        <Row xs={1} md={2} className="m-2">
+      <Row xs={1} md={2} className="m-2">
+        <Movies showTools={isFullOption} listMovies={movies} />
 
-          <Movies showTools={isFullOption} listMovies={movies} />
-
-          <Col className="m-3">
-            { isFullOption && <AddButton contentLink={contentLink} /> }
-          </Col>
-
-        </Row>
-
+        <Col className="m-3">
+          {isFullOption && <AddButton contentLink={contentLink} />}
+        </Col>
+      </Row>
     </div>
-  )
+  );
 };
